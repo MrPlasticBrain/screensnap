af78b9758e2a9659d9f9e993e0bdf8a6ea3a8fc0
mrplasticbrain <alekmilanov.sr@gmail.com>
2018-02-07 22:18:57 +0100

    add verson 2 of application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ImageUploadModule } from "angular2-image-upload";
+import { AlertModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +11,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ImageUploadModule.forRoot(),
+    AlertModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
